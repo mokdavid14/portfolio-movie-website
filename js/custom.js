c31a4638ddd311921756d4c13457caf5a78818c8
mokdavid14 <mokdavid14@gmail.com>
2014-06-25 16:05:03 +1200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,7 @@
 //Custom JQuery
 
+//Original idea was to have the Cinema Locations iframe collapsed, but since this was buggy for Firefox, dropped
 $(document).ready(function(){
 
-	$('.collapse').collapse();
+	// $('.collapse').collapse();
 });
